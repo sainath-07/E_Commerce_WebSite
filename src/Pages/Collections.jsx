7f277711983c9127c7810dbaaf557276646b6523
sainath-07 <sainathb308@gmail.com>
2024-06-10 18:47:27 +0530

    done

--- src/Pages/Collections.jsx
+++ src/Pages/Collections.jsx
@@ -1,9 +1,11 @@
-const Collections=()=>{
- return(
-    <>
-    <h1>Collection page,</h1>
-    </>
- )
-}
 
-export default Collections
+
+const Collections=()=>{
+  return(
+     <>
+     <h1>Collections</h1>
+     </>
+  )
+ }
+ 
+ export default Collections
