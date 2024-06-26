ab9f2e39691609e63ea55e27f8fc0bc60df9762d
sainath-07 <sainathb308@gmail.com>
2024-06-26 12:02:09 +0530

    wifi lucide icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Detector } from 'react-detect-offline';
 import './App.css';
 import NavigationStack from './Components/Navigationstack/navigation';
+import { WifiOff } from 'lucide-react';
 
 
 
@@ -25,7 +26,7 @@ function App() {
             <div className='flex flex-col border-2 w-full h-screen justify-center items-center'>
               <div>
 
-                <img width="50" height="50" src="https://img.icons8.com/ios-filled/50/wi-fi-disconnected.png" alt="wi-fi-disconnected" />
+              <WifiOff/>
               </div>
               <p>No internet Connection</p>
               <p>Please make sure you connect to Internet</p>
